4499e070704637392cce0f5676d509f18fa575ed
Johnson <johnson@kalakendradao.org>
2024-09-18 11:18:17 +0530

    moved app.js to api folder

--- api/app.js
+++ api/app.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
 
-const profileRouter = require('./routes/profile');
-const postRouter = require('./routes/post');
+const profileRouter = require('../routes/profile');
+const postRouter = require('../routes/post');
 
 app.use('/api/profile', profileRouter);
 app.use('/api/post', postRouter);
