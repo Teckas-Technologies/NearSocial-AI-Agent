8f10fcac2116223dbaf0fca3ff3804e46ccc4c25
Johnson <johnson@kalakendradao.org>
2024-09-18 11:39:51 +0530

    moved app.js to api folder

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
 
-const profileRouter = require('../routes/profile');
-const postRouter = require('../routes/post');
+const profileRouter = require('./routes/profile');
+const postRouter = require('./routes/post');
 
 app.use('/api/profile', profileRouter);
 app.use('/api/post', postRouter);
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(9000, () => {
     console.log("AI Agent Running on port : 9000")
