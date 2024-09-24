c8401b3f81a8f303b382eac6400964aed5029d27
Johnson <johnson@kalakendradao.org>
2024-09-24 15:44:21 +0530

    added new text

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const followRouter = require('./routes/follow');
 app.use('/api/profile', profileRouter);
 app.use('/api/post', postRouter);
 app.use('/api/follow', followRouter);
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Azure"));
 app.get('/.well-known/ai-plugin.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.sendFile(path.join(__dirname, 'public/.well-known/ai-plugin.json'));
